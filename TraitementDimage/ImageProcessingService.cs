fc4d347392c561196c2a6e1bbd201655fba8cf7b
charbel-elhajj <charbelelhajj99@hotmail.com>
2021-01-16 21:21:35 +0100

    addition sostraction

--- TraitementDimage/ImageProcessingService.cs
+++ TraitementDimage/ImageProcessingService.cs
@@ -9,13 +9,13 @@ namespace TraitementDimage
 {
     public class ImageProcessingService
     {
-        
+        public static int ONE = 255;
+        public static int TWO = 0;
 
         public static Bitmap ConvertBitmapToGrayscale(Bitmap bitmap)
         {
             // Make a Bitmap24 object.
             Bitmap bm = new Bitmap(bitmap);
-            
 
             // Process the pixels.
             for (int x = 0; x < bitmap.Width; x++)
@@ -38,7 +38,6 @@ namespace TraitementDimage
             // Make a Bitmap24 object.
             Bitmap bm = new Bitmap(bitmap);
 
-
             // Process the pixels.
             for (int x = 0; x < bitmap.Width; x++)
             {
@@ -55,6 +54,81 @@ namespace TraitementDimage
                         color1 = Color.Black;
                     }
                     bm.SetPixel(x, y, color1);
+                }
+            }
+
+            // Return bitmap
+            return bm;
+        }
+
+        public static Bitmap Addition(Bitmap bitmap1, Bitmap bitmap2)
+        {
+            // Make a Bitmap24 object.
+            Bitmap bm = new Bitmap(bitmap1);
+
+            // Process the pixels.
+            for (int x = 0; x < bitmap1.Width; x++)
+            {
+                for (int y = 0; y < bitmap1.Height; y++)
+                {
+                    Color color1 = bitmap1.GetPixel(x, y);
+                    Color color2 = bitmap2.GetPixel(x, y);
+                    int red = Math.Min(color1.R + color2.R, 255);
+                    int green = Math.Min(color1.G + color2.G, 255);
+                    int blue = Math.Min(color1.B + color2.B, 255);
+
+                    Color color = Color.FromArgb(red, green, blue);
+
+                    bm.SetPixel(x, y, color);
+                }
+            }
+
+            // Return bitmap
+            return bm;
+        }
+
+        public static Bitmap Substraction(Bitmap bitmap1, Bitmap bitmap2)
+        {
+            // Make a Bitmap24 object.
+            Bitmap bm = new Bitmap(bitmap1);
+
+            // Process the pixels.
+            for (int x = 0; x < bitmap1.Width; x++)
+            {
+                for (int y = 0; y < bitmap1.Height; y++)
+                {
+                    Color color1 = bitmap1.GetPixel(x, y);
+                    Color color2 = bitmap2.GetPixel(x, y);
+                    int red = Math.Max(color1.R - color2.R, 0);
+                    int green = Math.Max(color1.G - color2.G, 0);
+                    int blue = Math.Max(color1.B - color2.B, 0);
+
+                    Color color = Color.FromArgb(red, green, blue);
+
+                    bm.SetPixel(x, y, color);
+                }
+            }
+
+            // Return bitmap
+            return bm;
+        }
+
+        public static Bitmap ErosionCarre(Bitmap bitmap, int [][] elt, int width, int height)
+        {
+            Bitmap bm = new Bitmap(bitmap);
+
+            // Process the pixels.
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    bool isOne = true;
+                    for (int i = 0; i<width; i++)
+                    {
+                        
+                    }
+
+                    //bm.SetPixel(x, y, color);
                 }
             }
 
