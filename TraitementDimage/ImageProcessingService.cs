37616e98ef8d24df1e7f3951d8a968bf737e45bb
charbel-elhajj <charbelelhajj99@hotmail.com>
2021-01-16 23:45:23 +0100

    Errosion dillatation carre

--- TraitementDimage/ImageProcessingService.cs
+++ TraitementDimage/ImageProcessingService.cs
@@ -10,7 +10,19 @@ namespace TraitementDimage
     public class ImageProcessingService
     {
         public static int ONE = 255;
-        public static int TWO = 0;
+        public static int ZERO = 0;
+
+        public static void SetWhiteBackground() 
+        {
+            ONE = 0;
+            ZERO = 255;
+        }
+
+        public static void SetBlackBackground()
+        {
+            ONE = 255;
+            ZERO = 0;
+        }
 
         public static Bitmap ConvertBitmapToGrayscale(Bitmap bitmap)
         {
@@ -22,9 +34,9 @@ namespace TraitementDimage
             {
                 for (int y = 0; y < bitmap.Height; y++)
                 {
-                    Color color = bm.GetPixel(x,y);
+                    Color color = bm.GetPixel(x, y);
                     int average = (color.R + color.G + color.B) / 3;
-                    Color gray = Color.FromArgb(average,average,average);
+                    Color gray = Color.FromArgb(average, average, average);
                     bm.SetPixel(x, y, gray);
                 }
             }
@@ -113,27 +125,161 @@ namespace TraitementDimage
             return bm;
         }
 
-        public static Bitmap ErosionCarre(Bitmap bitmap, int [][] elt, int width, int height)
+        public static Bitmap ErosionCarre(Bitmap bitmap, int[][] elt, int taille)
         {
             Bitmap bm = new Bitmap(bitmap);
+            int somme;
+            int goal = 0;
+            int width = taille * 2 + 1;
+            int height = width;
 
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (elt[i][j] == ONE)
+                    {
+                        goal++;
+                    }
+                }
+            }
             // Process the pixels.
             for (int x = 0; x < bitmap.Width; x++)
             {
                 for (int y = 0; y < bitmap.Height; y++)
                 {
-                    bool isOne = true;
-                    for (int i = 0; i<width; i++)
+                    somme = 0;
+                    for (int i = 0; i < width; i++)
                     {
-                        
+                        for (int j = 0; j < height; j++)
+                        {
+                            if (elt[i][j] == ONE)
+                            {
+                                try
+                                {
+                                    if (bitmap.GetPixel(x - width + i, y - height + j).R == ONE)
+                                    {
+                                        somme++;
+                                    }
+                                }
+                                catch (Exception ex) { }
+                            }
+                        }
                     }
-
-                    //bm.SetPixel(x, y, color);
+                    if (somme == goal)
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(ONE, ONE, ONE));
+                    }
+                    else
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(ZERO, ZERO, ZERO));
+                    }
                 }
             }
 
             // Return bitmap
             return bm;
         }
+        public static Bitmap DillatationCarre(Bitmap bitmap, int[][] elt, int taille)
+        {
+            Bitmap bm = new Bitmap(bitmap);
+            int somme;
+            int width = taille * 2 + 1;
+            int height = width;
+
+            // Process the pixels.
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    somme = 0;
+                    for (int i = 0; i < width; i++)
+                    {
+                        for (int j = 0; j < height; j++)
+                        {
+                            if (elt[i][j] == ONE)
+                            {
+                                try
+                                {
+                                    if (bitmap.GetPixel(x - width + i, y - height + j).R == ONE)
+                                    {
+                                        somme++;
+                                    }
+                                }
+                                catch (Exception ex) { }
+                            }
+                        }
+                    }
+                    if (somme > 0)
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(ONE, ONE, ONE));
+                    }
+                    else
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(ZERO, ZERO, ZERO));
+                    }
+
+
+                }
+            }
+            return bm;
+        }
+        public static Bitmap ErosionHex(Bitmap bitmap, int[][] elt, int taille)
+        {
+            Bitmap bm = new Bitmap(bitmap);
+            int somme;
+            int goal = 0;
+            int width = taille * 2 + 1;
+            int height = width;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (elt[i][j] == ONE)
+                    {
+                        goal++;
+                    }
+                }
+            }
+            // Process the pixels.
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    somme = 0;
+                    for (int i = 0; i < width; i++)
+                    {
+                        for (int j = 0; j < height; j++)
+                        {
+                            if (elt[i][j] == ONE)
+                            {
+                                try
+                                {
+                                    if (bitmap.GetPixel(x - width + i, y - height + j).R == ONE)
+                                    {
+                                        somme++;
+                                    }
+                                }
+                                catch (Exception ex) { }
+                            }
+                        }
+                    }
+                    if (somme == goal)
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(ONE, ONE, ONE));
+                    }
+                    else
+                    {
+                        bm.SetPixel(x, y, Color.FromArgb(ZERO, ZERO, ZERO));
+                    }
+                }
+            }
+
+            // Return bitmap
+            return bm;
+        }
+
+
     }
 }
